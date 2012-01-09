00c41a9c071cde92641b9fcf04006d5d849799b3
smeest1 <smeest1@gmail.com>
2012-01-09 20:03:25 +0000

    added tag directory

---
+++ gridfieldsclib-0.7/src/outputtuples.h
@@ -0,0 +1,31 @@
+#ifndef _OUTPUTTUPLES_H
+#define _OUTPUTTUPLES_H
+
+#include "gridfieldoperator.h"
+//#include "netcdfcpp.h"
+#include <iostream>
+
+class OutputTuplesOp : public UnaryGridFieldOperator {
+public:
+  OutputTuplesOp(string fn, long off, GridFieldOperator *Op);
+  OutputTuplesOp(string fn, GridFieldOperator *Op);
+  
+  int offset;
+  string filename;
+  void Execute();
+  void setFileName(char *fn) { filename = string(fn); };
+  static void Output(GridField *GF, string filename, long offset);
+  //static void WriteNetCDF(vector<GridField *> &dims, GridField *cross);
+
+private:
+
+  static void writeCellArray(AbstractCellArray *ca, ofstream &f);
+  static void writeGrid(Grid *G, ofstream &f);
+  static void writeGridField(GridField *GF, ofstream &f);
+  static void writeDataset(const Dataset &ds, ofstream &f);
+  static void writeName(string name, ofstream &f);
+
+  //static NcVar *putData(Array *a, NcFile *ncdf, long *counts, NcDim **d, int dimcount);
+};
+
+#endif
---
+++ pygridfields-0.7/clib/tests/gfbindvar.cc
@@ -0,0 +1,64 @@
+#include "gridfield.h"
+#include "bind.h"
+#include "scaninternal.h"
+#include "output.h"
+#include "elcircfile.h"
+#include "gfops.h"
+
+string filename("1_salt.63");
+string opid;
+string attr;
+string posattr;
+string sourcefilename;
+int timestep;
+
+int main( int argc, char *argv[] ) {
+  
+  cout << "gfBind..." << flush;
+  checkopts( argc, argv );
+  GridField *gf = ScanInternal::Scan(filename, 0);
+  ElcircFile *f = new ElcircFile(sourcefilename);
+  ArrayReader *ar = f->getVariableReader(timestep, posattr);
+  GridFieldOperator *b = 
+        new BindOp::BindOp(attr, FLOAT, ar, gf);
+
+  GridFieldOperator *output = new OutputOp::OutputOp(filename+opid, 0, b);
+  output->Execute();
+  cout << "done." << endl;
+  
+}
+
+void checkopts( int argc, char **argv )
+{
+        CCmdLine cmdline;
+        if (cmdline.SplitLine(argc, argv) < 1) {
+          showhelp();
+          exit(-1);
+        }
+        
+        try {
+          filename = cmdline.GetArgument("-f", 0);
+        } catch (...) {
+          showhelp();
+          exit(-1);
+        }
+        
+        from_string<string>(opid, cmdline.GetSafeArgument("-i", 0, "_B"));
+        from_string<string>(attr, cmdline.GetSafeArgument("-a", 0, "var"));
+        from_string<string>(posattr, cmdline.GetSafeArgument("-p", 0, ""));
+        from_string<int>(timestep, cmdline.GetSafeArgument("-t", 0, "0"));
+        from_string<string>(sourcefilename, cmdline.GetSafeArgument("-s", 0, filename.c_str()));
+}
+
+void showhelp() {
+  cout 
+    << "Usage: "
+    << endl << "gfbind -f <previous output filename> [-a <attribute name>]"
+    << endl << "                                     [-p <position attribute>]"
+    << endl << "                                     [-t <timestep>]"
+    << endl << "                                     [-s <source filename>]"
+
+    << endl << endl 
+    << "Ex: gfbind -f 1_salt.63_S_A -a salt -p addr -t 34 -s 2_salt.63"
+    << endl;
+}
