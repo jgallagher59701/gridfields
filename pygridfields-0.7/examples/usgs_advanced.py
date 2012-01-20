dcb256097e1996c328ee2589846675ac3e5983d7
smeest1 <smeest1@gmail.com>
2012-01-20 00:09:01 +0000

    added usgs_advanced

--- pygridfields-0.7/examples/usgs_advanced.py
+++ pygridfields-0.7/examples/usgs_advanced.py
@@ -4,7 +4,8 @@
 
 #
 #
-import os.path, sys
+import matplotlib.pyplot as plt
+from matplotlib import tri
 
 import numpy as np
 import netCDF4
@@ -21,27 +22,20 @@ URL = 'http://geoport.whoi.edu/thredds/dodsC/usgs/data1/rsignell/models/adcirc/f
 # ZeroGrid is syntactic sugar for a zero dimensional grid
 # A 0-d grid is a grid consisting of points only
 
+nc = netCDF4.Dataset(URL)
+x=nc.variables['x']
+y=nc.variables['y']
+# read water depth at nodes
+h=nc.variables['depth']
+# read connectivity array
+nv=nc.variables['element']
+x1=x[:]
+y1=y[:]
+h1=h[:]
+nv=nv[:]-1
 
-try:
-    nc = netCDF4.Dataset(URL)
-    x=nc.variables['x']
-    y=nc.variables['y']
-    # read water depth at nodes
-    h=nc.variables['depth']
-    # read connectivity array
-    nv=nc.variables['element']
-    x1=x[:]
-    y1=y[:]
-    h1=h[:]
-    nv=nv[:]-1
-    print "hi"
-except:
-    N = 50
-    x1 = np.random.random(N)
-    y1 = np.random.random(N)
-    h1 = np.random.random(N)
 NumPhysNodes=len(x1)
-print nv[1][:]
+#print nv[1][:]
 import gridfield.core as gf
 from gridfield.algebra import Apply, Restrict, Wrap
 #g = gf.ZeroGrid("points",NumPhysNodes)
@@ -53,7 +47,7 @@ NumPhysElems=len(nv)
 for i in range(1,NumPhysElems+1):
  twocells.addCellNodes(nv[i-1][:], 3)
 
-print NumPhysNodes
+# print NumPhysNodes
 # construct an array of X-values
 
 x = gf.Array("x", gf.FLOAT, NumPhysNodes)
@@ -99,7 +93,15 @@ G = Wrap(G)
 #H.show()
 
 # restrict using the data at rank 0
-rG = Restrict("abs(h)>=0.0", 0, G)
+rG = Restrict("abs(h)<=500.0", 0, G)
+
+# restrict to lon/lat range
+bounds = (-71.5,-63.0,39.5,46.0)
+condition = "(%s<x) & (x<%s) & (%s<y) & (y<%s)" % bounds
+
+# Apply the condition to the vertices
+print "Apply condition %s" % condition
+rG = Restrict(condition, 0, rG)
 
 # execute the recipe
 Result = rG.getResult()
@@ -111,24 +113,33 @@ ax.show()
 
 print "Attributes of vertices: ", Result.GetScheme(0).asString()
 
+# Gridfields x,y,z to Numpy
+d0 = Result.GetDataset(0)
+lstvals = [gf.derefFloat(d0.GetAttributeVal("x", i)) for i in range(d0.Size())]
+x = np.array(lstvals)
+lstvals = [gf.derefFloat(d0.GetAttributeVal("y", i)) for i in range(d0.Size())]
+y = np.array(lstvals)
+lstvals = [gf.derefFloat(d0.GetAttributeVal("h", i)) for i in range(d0.Size())]
+h = np.array(lstvals)
+
+# Gridfields ele connectivity array to Numpy
+newG   = Result.GetGrid()
+newG.normalize()   # reindex the connectiviy array to the restricted grid
+twocells = gf.castToCellArray(newG.getKCells(2))
+ele=np.zeros((twocells.getsize(), 3))
+
+for i in range(twocells.getsize()):
+ cell = twocells.getCell(i)
+ ele[i][:]=[cell.getnode(0), cell.getnode(1), cell.getnode(2)]
 
 Gp=Result.GetScheme(0)
-try:
-  import vtk
-except:
-  print "VTK not installed -- no visualization attempted"
-  sys.exit()
 
-import gridfield.vis as vis
-from gridfield.vis import ToVTK
 
-# Show the gridfield, making whatever assumptions necessary
-# color by the "h" attribute
+t=tri.Triangulation(x,y,triangles=ele)
 
-vis.ToVTK(Result, "h", show=True,save=True, capture=True, fname="vis.png")
-
-#vis.Visualize(ax, "q")
-
-# To save the image to a file and not interact with it, do this:
-# vis.ToVTK(rH, "h", capture=True, fname="depth.png")
-
+plt.figure(1)
+plt.tricontour(t, h, 15, linewidths=0.5, colors='k')
+plt.tricontourf(t, h, 15, cmap=plt.cm.jet)
+plt.title('tri.Triangulation: tricontour ')
+plt.colorbar()
+plt.show()
